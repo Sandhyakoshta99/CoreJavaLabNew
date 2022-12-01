f78b50f0b5688ead59d435270f2c1adc7dd43049
sandhya koshta <sandhyakoshta44@gmail.com>
2022-12-01 13:57:58 +0530

    Done Ma'am

---
+++ 01Dec2022LabTest/src/com/Question2/TraficLight.java
@@ -0,0 +1,56 @@
+/*
+The application must fulfill the following requirements:
+Initially, when the application is executed, only the RED light must be ON.
+After 3 seconds, the RED light should turn off and only the GREEN light should turn ON for 5 seconds.
+Then, the GREEN light should turn off and only the YELLOW light should turn ON for 2 seconds.
+Then, the YELLOW light should turn to RED.
+The preceding process should continue, infinitely.
+You also need to display the remaining time for each light.
+
+ *@AUTHOR : SANDHYA KOSHTA
+ *@DATE : 01 December 2022
+ */
+
+package com.Question2;
+public class TraficLight extends Thread{
+	public static void main(String[] args) {
+				// TODO Auto-generated method stub
+				Thread A = new Thread();
+				Thread B = new Thread();
+				Thread C = new Thread();
+				System.out.println("-----------------------------------------------------");
+				System.out.println("                 RED LIGHT IS ON                     ");
+				System.out.println("-----------------------------------------------------");
+				A.start();
+				try {
+					Thread.sleep(3000);
+					System.out.println("_________________________________________");
+					System.out.println("RED LIGHT IS OFF");
+					System.out.println("GREEN LIGHT IS ON");
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				B.start();
+				try {
+					Thread.sleep(8000);
+					System.out.println("_________________________________________");
+					System.out.println("GREEN LIGHT IS OFF");
+					System.out.println("YELLOW LIGHT IS ON");
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				C.start();
+				try {
+					Thread.sleep(10000);
+					System.out.println("_________________________________________");
+					System.out.println("RED LIGHT IS ON");
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	
+	}
+
+}
